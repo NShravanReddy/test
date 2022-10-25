d6e67cab532f4980f3b16d15691cf38d9b7d102a
shravan <nshravanreddy6@gmail.com>
2022-10-25 11:17:05 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+function Header(){
+return(
+  <header className='App-header'>
+<nav className='nav '>
+        <img className="nav-logo" src={logo} alt='react' width="40px"/>
+          <div >
+              <ul className='nav-items'>
+                <li>Pricing</li>
+                <li>About</li>
+                <li>Contact</li>
+              </ul>
+          </div>
+</nav>
+</header>
+)
+}
+function MainContent(){
+  return(
+<div className='App'>
+
+  <h1>Top Reasons to learn react</h1>
+    <ol>Reasons to learn react
+    <li>Open source</li>
+    <li>Easy to understand</li>
+    </ol>
+    </div>
+  )
+}
+function App(){
+  return(
+<div>
+  <Header/>
+  <MainContent/>
+
+  <Footer/>
+</div>
+
+  )
+}
+function Footer(){
+  return(
+    <footer>@ 2022 n3techs. All rights reserved.</footer>
+  )
+}
+export default  App;
